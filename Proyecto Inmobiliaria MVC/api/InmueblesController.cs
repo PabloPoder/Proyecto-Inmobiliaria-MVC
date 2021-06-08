e38a772eca32d66caeb561d3b30758a89aef4be3
PabloPoder <pablo.poder.mail@gmail.com>
2021-06-08 16:18:50 -0300

    Carpeta Api

--- Proyecto Inmobiliaria MVC/api/InmueblesController.cs
+++ Proyecto Inmobiliaria MVC/api/InmueblesController.cs
@@ -12,7 +12,7 @@ using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Proyecto_Inmobiliaria_MVC.Api
+namespace Proyecto_Inmobiliaria_MVC.api
 {
     [Route("api/[controller]")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
